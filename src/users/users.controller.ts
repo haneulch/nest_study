fd616655ce4ce24301ac91c50ba2298857b6d796
haneulch <khaneulc@kakao.com>
2022-02-10 16:38:41 +0900

    gitignore 변경 및 코드 정리

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,19 +11,16 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Public()
-  @Post('/create')
+  @Post('create')
   async create(@Req() request) {
     const result = await this.usersService.create(request.body);
     if (result) {
       return ResponseUtils.success();
     }
-    return ResponseUtils.error(
-      ResultCode.FGP_0001.toString(),
-      MessageConstant.DUPLICATE_USERNAME,
-    );
+    return ResponseUtils.error(ResultCode.FGP_0001.toString(), MessageConstant.DUPLICATE_USERNAME);
   }
 
-  @Get('/:username')
+  @Get(':username')
   async getUserList(@Req() request): Promise<ResponseDto> {
     const data = await this.usersService.findOne(request.params.username);
     return ResponseUtils.success('', data);
